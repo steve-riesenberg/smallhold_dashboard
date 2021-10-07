814ccd670d325cd711b1894a4f99ad4c1d3d933b
unknown <steve.riesenberg@gmail.com>
2021-10-07 09:57:00 -0400

    Trying full website with original server config

--- app.py
+++ app.py
@@ -89,10 +89,10 @@ humidity_fig = make_time_scatter(humidity, title='<b>Humidity Percentage</b>')
 # start App
 ##################################################################
 
-server = flask.Flask(__name__)
-app = dash.Dash(external_stylesheets=external_stylesheets, server=server)
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-# server = app.server
+# server = flask.Flask(__name__)
+# app = dash.Dash(external_stylesheets=external_stylesheets, server=server)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div(children=[
     # Page Title
@@ -146,65 +146,65 @@ app.layout = html.Div(children=[
         style={'width': '100%', 'display': 'inline-block'})
 ])
 
-# @app.callback(Output('co2-graph', 'figure'),
-#             Output('temp-graph', 'figure'),
-#             Output('humidity-graph', 'figure'),
-#             Input('btn-24-Hours', 'n_clicks'),
-#             Input('btn-3-Days', 'n_clicks'),
-#             Input('btn-1-Week', 'n_clicks'), 
-#             Input('btn-All-Time', 'n_clicks'))
-# def update_figure(btn1, btn2, btn3, btn4):
-#     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-#     data_list = {'co2':co2, 'temp':temp, 'humidity':humidity}
-#     max_date_dict = {dfname:df['date_time'].max() for dfname, df in data_list.items()}
-#     if 'btn-24-Hours' in changed_id:
-#         min_date_dict = {dfname:max_date_dict[dfname] - datetime.timedelta(days=1) for dfname, df in data_list.items()}
-#     elif 'btn-3-Days' in changed_id:
-#         min_date_dict = {dfname:max_date_dict[dfname] - datetime.timedelta(days=3) for dfname, df in data_list.items()}
-#     elif 'btn-1-Week' in changed_id:
-#         min_date_dict = {dfname:max_date_dict[dfname] - datetime.timedelta(days=7) for dfname, df in data_list.items()}
-#     elif 'btn-All-Time' in changed_id:
-#         min_date_dict = {dfname:df['date_time'].min() for dfname, df in data_list.items()}
-#     else:
-#         min_date_dict = {dfname:max_date_dict[dfname] - datetime.timedelta(days=1) for dfname, df in data_list.items()}
+@app.callback(Output('co2-graph', 'figure'),
+            Output('temp-graph', 'figure'),
+            Output('humidity-graph', 'figure'),
+            Input('btn-24-Hours', 'n_clicks'),
+            Input('btn-3-Days', 'n_clicks'),
+            Input('btn-1-Week', 'n_clicks'), 
+            Input('btn-All-Time', 'n_clicks'))
+def update_figure(btn1, btn2, btn3, btn4):
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    data_list = {'co2':co2, 'temp':temp, 'humidity':humidity}
+    max_date_dict = {dfname:df['date_time'].max() for dfname, df in data_list.items()}
+    if 'btn-24-Hours' in changed_id:
+        min_date_dict = {dfname:max_date_dict[dfname] - datetime.timedelta(days=1) for dfname, df in data_list.items()}
+    elif 'btn-3-Days' in changed_id:
+        min_date_dict = {dfname:max_date_dict[dfname] - datetime.timedelta(days=3) for dfname, df in data_list.items()}
+    elif 'btn-1-Week' in changed_id:
+        min_date_dict = {dfname:max_date_dict[dfname] - datetime.timedelta(days=7) for dfname, df in data_list.items()}
+    elif 'btn-All-Time' in changed_id:
+        min_date_dict = {dfname:df['date_time'].min() for dfname, df in data_list.items()}
+    else:
+        min_date_dict = {dfname:max_date_dict[dfname] - datetime.timedelta(days=1) for dfname, df in data_list.items()}
     
-#     co2_button_filter = (co2['date_time'] <= max_date_dict['co2']) & (co2['date_time'] >= min_date_dict['co2'])
-#     temp_button_filter = (temp['date_time'] <= max_date_dict['temp']) & (temp['date_time'] >= min_date_dict['temp'])
-#     humidity_button_filter = (humidity['date_time'] <= max_date_dict['humidity']) & (humidity['date_time'] >= min_date_dict['humidity'])
-#     co2_filtered = co2[co2_button_filter]
-#     temp_filtered = temp[temp_button_filter]
-#     humidity_filtered = humidity[humidity_button_filter]
-#     co2_fig = make_time_scatter(co2_filtered, title='<b>CO2 PPM</b>')
-#     temp_fig = make_time_scatter(temp_filtered, title='<b>Temperature in Degrees Fahrenheit</b>')
-#     humidity_fig = make_time_scatter(humidity_filtered, title='<b>Humidity Percentage</b>')
-#     # co2_fig.update_layout(transition_duration=500)
-#     # temp_fig.update_layout(transition_duration=500)
-#     # humidity_fig.update_layout(transition_duration=500)
+    co2_button_filter = (co2['date_time'] <= max_date_dict['co2']) & (co2['date_time'] >= min_date_dict['co2'])
+    temp_button_filter = (temp['date_time'] <= max_date_dict['temp']) & (temp['date_time'] >= min_date_dict['temp'])
+    humidity_button_filter = (humidity['date_time'] <= max_date_dict['humidity']) & (humidity['date_time'] >= min_date_dict['humidity'])
+    co2_filtered = co2[co2_button_filter]
+    temp_filtered = temp[temp_button_filter]
+    humidity_filtered = humidity[humidity_button_filter]
+    co2_fig = make_time_scatter(co2_filtered, title='<b>CO2 PPM</b>')
+    temp_fig = make_time_scatter(temp_filtered, title='<b>Temperature in Degrees Fahrenheit</b>')
+    humidity_fig = make_time_scatter(humidity_filtered, title='<b>Humidity Percentage</b>')
+    # co2_fig.update_layout(transition_duration=500)
+    # temp_fig.update_layout(transition_duration=500)
+    # humidity_fig.update_layout(transition_duration=500)
 
-#     return co2_fig, temp_fig, humidity_fig
+    return co2_fig, temp_fig, humidity_fig
 
-# @app.callback(Output('btn-24-Hours', 'style'),
-#     Output('btn-3-Days', 'style'),
-#     Output('btn-1-Week', 'style'),
-#     Output('btn-All-Time', 'style'),
-#     Input('btn-24-Hours', 'n_clicks'),
-#     Input('btn-3-Days', 'n_clicks'),
-#     Input('btn-1-Week', 'n_clicks'), 
-#     Input('btn-All-Time', 'n_clicks'))
-# def change_button_style(btn1, btn2, btn3, btn4):
-#     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-#     style_24, style3, style1, styleall = white_button_style, white_button_style, white_button_style, white_button_style
-#     if 'btn-24-Hours' in changed_id:
-#         style_24 = blue_button_style
-#     elif 'btn-3-Days' in changed_id:
-#         style3 = blue_button_style
-#     elif 'btn-1-Week' in changed_id:
-#         style1 = blue_button_style
-#     elif 'btn-All-Time' in changed_id:
-#         styleall = blue_button_style
-#     else:
-#         style_24 = blue_button_style
-#     return style_24, style3, style1, styleall
+@app.callback(Output('btn-24-Hours', 'style'),
+    Output('btn-3-Days', 'style'),
+    Output('btn-1-Week', 'style'),
+    Output('btn-All-Time', 'style'),
+    Input('btn-24-Hours', 'n_clicks'),
+    Input('btn-3-Days', 'n_clicks'),
+    Input('btn-1-Week', 'n_clicks'), 
+    Input('btn-All-Time', 'n_clicks'))
+def change_button_style(btn1, btn2, btn3, btn4):
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    style_24, style3, style1, styleall = white_button_style, white_button_style, white_button_style, white_button_style
+    if 'btn-24-Hours' in changed_id:
+        style_24 = blue_button_style
+    elif 'btn-3-Days' in changed_id:
+        style3 = blue_button_style
+    elif 'btn-1-Week' in changed_id:
+        style1 = blue_button_style
+    elif 'btn-All-Time' in changed_id:
+        styleall = blue_button_style
+    else:
+        style_24 = blue_button_style
+    return style_24, style3, style1, styleall
 
 if __name__ == '__main__':
     app.run_server(debug=True)
